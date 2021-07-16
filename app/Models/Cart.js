7aa59f17ccc53b78f2adfcd1be9c7d85e105af99
cjgreenie <seejarom@gmail.com>
2021-07-15 22:21:29 -0600

    working on cart

--- app/Models/Cart.js
+++ app/Models/Cart.js
@@ -1,28 +1,43 @@
 export default class Cart {
 
-   //filter function on the animals array to return an array of ONLY the animals that have a "cart" value above 0, and then make a function that draws the template for cart-ed animals
-   get Template() {
+   constructor() {
+
+      function FilterCart() {
+         aSubtotal = []
+         animals.forEach(subtotal => template += subtotal.Template)
+         if (aSubtotal > 0)
+            return aSubtotal;
+
+
+
+      }
+
+
+      //filter function on the animals array to return an array of ONLY the animals that have a "cart" value above 0, and then make a function that draws the template for cart-ed animals
+
 
    }
+
+
+   //CART ITEMS:
+   get Template() {
+      return `
+   <div class="col-12 p-1 border">
+   <div class="row justify-content-between ml-1">
+       <div class="col cart-img" style="background-image: url('${this.imgUrl}')"></div>
+       <div class="col-3 d-flex align-items-end">
+           <h3>${this.name}</h3>
+       </div>
+       <div class="col-3  d-flex align-items-end text-">
+           <h6><em>CART: ${this.cart}</em></h6>
+       </div>
+       <div class="col-3 text-primary d-flex align-items-end">
+           <h5>SUBTOTAL: ${this.subtotal}</h5>
+       </div>
+   </div>
+</div>`
+   }
 }
-
-//CART ITEMS:
-// <div class="col-12 p-1 border">
-//                 <div class="row justify-content-between ml-1">
-//                     <div class="col cart-img" style="background-image: url(./assets/img/coffee\ cup.jpg)"></div>
-//                     <div class="col-3 d-flex align-items-end">
-//                         <h3>TITLE</h3>
-//                     </div>
-//                     <div class="col-3  d-flex align-items-end text-">
-//                         <h6><em>CART: 1</em></h6>
-//                     </div>
-//                     <div class="col-3 text-primary d-flex align-items-end">
-//                         <h5>SUBTOTAL</h5>
-//                     </div>
-//                 </div>
-//             </div>
-
-
 //CART TOTAL:
 /* <div class="col-12 text-right">
                 <h3 class="px-3"><b>TOTAL: </b></h3>
---
+++ app/Services/AnimalsService.js
@@ -0,0 +1,19 @@
+import { ProxyState } from "../AppState";
+import Animal from "../Models/Animal.js";
+import Cart from "../Models/Cart";
+
+class AnimalsService {
+
+
+
+
+
+
+
+}
+
+
+
+
+export const animalsService = new AnimalsService()
+
