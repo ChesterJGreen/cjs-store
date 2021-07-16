f8906f094db561948df7149815431a9951b565a3
cjgreenie <seejarom@gmail.com>
2021-07-15 21:26:38 -0600

    getting it going

--- app/Models/Animal.js
+++ app/Models/Animal.js
@@ -4,19 +4,19 @@ import { generateId } from "../Utils/generateId.js"
 
 export default class Animal {
 
-   constructor(imgUrl, name, price, description, stock, cart = 0, id = generateId()) {
-      this.imgUrl = imgUrl
-      this.name = name
-      this.price = price.toLocaleString("en-US", { style: 'currency', currency: 'USD' })
-      this.description = description
-      this.stock = stock
-      this.id = id
-      this.cart = cart
-      this.subtotal = (cart * price).toLocaleString("en-US", { style: 'currency', currency: 'USD' })
-   }
+    constructor(imgUrl, name, price, description, stock, cart = 0, id = generateId()) {
+        this.imgUrl = imgUrl
+        this.name = name
+        this.price = price.toLocaleString("en-US", { style: 'currency', currency: 'USD' })
+        this.description = description
+        this.stock = stock
+        this.id = id
+        this.cart = cart
+        this.subtotal = (cart * price).toLocaleString("en-US", { style: 'currency', currency: 'USD' })
+    }
 
-   get Template() {
-      return `<div class="col-lg-3 col-md-6 mt-3">
+    get Template() {
+        return /*html*/`<div class="col-lg-3 col-md-6 mt-3">
       <div class="bg-light rounded shadow-light card p-2" id="${this.id}">
           <div class="product-img" style="background-image: url('${this.imgUrl}')"></div>
           <h4 class="text-center m-2"><b>${this.name} - ${this.price}</b></h4>
@@ -26,9 +26,9 @@ export default class Animal {
           </div>
       </div>
   </div>`
-   }
+    }
 
-   get CartTemplate() {
-
-   }
+    get CartTemplate() {
+        return ``
+    }
 }
