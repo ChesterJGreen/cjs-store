fd455916d18adec2ef14f305dafb0cc78368f965
cjgreenie <seejarom@gmail.com>
2021-07-15 22:36:30 -0600

    working on cart now

--- app/Controllers/CartController.js
+++ app/Controllers/CartController.js
@@ -1,6 +1,5 @@
-import { ProxyState } from "../AppState.js";
-import { animalsService } from "../Services/AnimalsService.js";
-import { loadState, saveState } from "../Utils/LocalStorage.js";
+
+
 //The next step is to have this iterate over the "animals" object in AppState, determine if any of them have a "cart" value higher than 0, and then drawing the cart based on the values of that object.
 
 export default class CartController {
